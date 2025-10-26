8ebf22f2eed187d9c80abcf75ea133e0cdc88369
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-10-26 22:04:56 +0530

    Code Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,25 +3,20 @@ import logo from '../assets/weather-logo.jpg';
 
 const Navbar = () => {
     return (
-        <header className="fixed top-[2%] left-1/2 -translate-x-1/2 w-[90%] 
-bg-linear-to-r from-blue-500/40 via-indigo-500/40 to-sky-500/40 
-backdrop-blur-md border border-white/20 shadow-lg 
-rounded-2xl p-3 flex items-center gap-4 z-50 transition-all duration-300 hover:shadow-xl">
-            <nav className="flex flex-row items-center gap-3">
-                <div>
-                    <img
-                        src={logo}
-                        alt="WeatherApp-Logo"
-                        className="h-10 w-20 object-contain bg-transparent mix-blend-screen drop-shadow-md transition-transform duration-300 hover:scale-105"
-                    />
-                </div>
-                <div>
-                    <h1 className="text-2xl sm:text-3xl font-semibold font-mono text-white drop-shadow-md">
-                        Weather<span className="text-yellow-300">App</span>
-                    </h1>
-                </div>
+        <header
+            className="fixed top-4 left-1/2 -translate-x-1/2 w-[92%] sm:w-[90%] bg-linear-to-r from-sky-500/40 via-blue-500/30 to-indigo-500/40 backdrop-blur-xl border border-white/25 shadow-lg rounded-2xl px-5 py-3 flex items-center justify-between   z-50 transition-all duration-300 hover:shadow-2xl hover:border-white/40">
+            <nav className="flex items-center gap-3 sm:gap-5 sm:flex-row justify-center-safe">
+                <img
+                    src={logo}
+                    alt="WeatherApp Logo"
+                    className="h-10 sm:h-12 w-auto object-contain drop-shadow-md transition-transform duration-300 hover:scale-110"
+                />
+                <h1 className="text-2xl sm:text-3xl font-bold font-mono text-white drop-shadow-lg tracking-wide">
+                    Weather<span className="text-yellow-300">App</span>
+                </h1>
             </nav>
         </header>
+
     );
 }
 
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -5,25 +5,28 @@ export default function WeatherCard({ weather }) {
 
     return (
         <>
-            <div className="mt-3 bg-linear-to-br from-[#e3f2fd] to-[#bbdefb] border border-gray-300 shadow-lg rounded-2xl p-6 sm:p-8 text-center text-black w-[90%] max-w-lg mx-auto transition-all duration-300 ease-in-out hover:shadow-2xl">
-                <h2 className="text-2xl sm:text-3xl font-bold mb-3">{weather.name}</h2>
+            <div className="mt-3 bg-linear-to-br from-[#e3f2fd] to-[#bbdefb] border border-gray-300 shadow-lg rounded-2xl p-6 sm:p-8 text-center text-black w-full max-w-3xl mx-auto transition-all duration-300 ease-in-out hover:shadow-2xl"
+            >
+                <h2 className="text-2xl sm:text-3xl font-bold mb-3">
+                    {weather.name}, <span className="text-xl text-gray-600">{weather.sys.country}</span>
+                </h2>
                 <p className="text-base sm:text-lg capitalize tracking-wide text-gray-700">
                     {weather.weather[0].description}
                 </p>
                 <h3 className="text-4xl sm:text-5xl font-extrabold mt-3 text-gray-900">
                     {Math.round(weather.main.temp)}°C
                 </h3>
-                <div className="flex flex-col sm:flex-row justify-center items-center gap-6 sm:gap-10 mt-6 text-sm sm:text-base">
+                <div className="flex flex-col sm:flex-row justify-center items-center gap-5 sm:gap-5 mt-6 text-sm sm:text-base">
                     <div className="bg-white/50 px-4 py-3 rounded-xl shadow-sm w-[80%] sm:w-auto">
-                        <p className="font-semibold">🌡️ Feels Like</p>
+                        <p className="font-semibold whitespace-nowrap">🌡️ Feels Like</p>
                         <p className="text-gray-800">{Math.round(weather.main.feels_like)}°C</p>
                     </div>
                     <div className="bg-white/50 px-4 py-3 rounded-xl shadow-sm w-[80%] sm:w-auto">
-                        <p className="font-semibold">💧 Humidity</p>
+                        <p className="font-semibold whitespace-nowrap">💧 Humidity</p>
                         <p className="text-gray-800">{weather.main.humidity}%</p>
                     </div>
                     <div className="bg-white/50 px-4 py-3 rounded-xl shadow-sm w-[80%] sm:w-auto">
-                        <p className="font-semibold">🌬️ Wind</p>
+                        <p className="font-semibold whitespace-nowrap">🌬️ Wind</p>
                         <p className="text-gray-800">{weather.wind.speed} m/s</p>
                     </div>
                 </div>
